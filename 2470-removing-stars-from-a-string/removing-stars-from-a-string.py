8d57fcb4e5b3799e4200b4fd815d3b28f74802d2
Hrishikesh Karande <hrishikeshkarande1997@gmail.com>
2025-06-07 19:38:28 +0200

    Time: 93 ms (72.05%) | Memory: 19.1 MB (37.13%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -1,5 +1,16 @@
 class Solution:
     def removeStars(self, s: str) -> str:
+        '''
+        1. Initialize an empty list called 'stack'
+        2. For each character 'ch' in the input string 's':
+                a. If 'ch' is not '*':
+                - Append 'ch' to 'stack'
+                b. Else (if 'ch' is '*'):
+                - Pop the last element from 'stack' (remove closest non-star character)
+        3. After processing all characters:
+                - Join the elements of 'stack' to form the final string
+        4. Return or print the final string
+        '''
         stack = []
 
         for ch in s:
